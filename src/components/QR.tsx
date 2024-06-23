bf88076c0e00bc39ccb26be8398922e32e0ab5d7
Trillium Smith <spiteless@gmail.com>
2024-06-23 01:17:42 -0700

    feat: Create medium screen bg/fg toggle

--- src/components/QR.tsx
+++ src/components/QR.tsx
@@ -40,6 +40,10 @@ function QR() {
 
   const qrRef = useRef(null);
 
+  const [bgFgSwitch, setBgFgSwitch] = useState(false);
+
+  const handleBgFgSwitch = () => setBgFgSwitch(!bgFgSwitch);
+
   const handleColorChangeBg = (color: { hex: string }) => {
     setColorBg(color.hex);
   };
@@ -79,8 +83,8 @@ function QR() {
   return (
     <div className="font-mono flex items-center flex-col bg-gradient-to-r from-blue-900 to-green-900 min-h-screen">
       <div className="flex items-center flex-col">
-        <div className="my-5 md:my-10">
-          <div className="flex items-center md:flex-row flex-col pb-10 md:pb-6">
+        <div className="my-5 md:my-10 hidden md:block">
+          <div className="flex items-center md:flex-row flex-col pb-10 md:pb-8 hidden md:flex">
             <div className="flex flex-col">
               <h2 className="text-2xl font-bold underline whitespace-pre">
                 Foreground Color: {colorFg.padEnd(9, " ")}
@@ -144,9 +148,127 @@ function QR() {
                 Reset Background to Transparent
               </button>
             </div>
-            <div className="ml-4 pt-4 md:p-0">
+            <div className="md:ml-4 ml-0 pt-4 md:p-0 flex flex-row">
+              <div className="flex flex-col md:hidden block items-stretch content-stretch bg-gray-100 min-h-full mr-4 md:mr-0">
+                <button className="h-1/2 bg-purple-500 w-36">Foreground</button>
+                <button className="h-1/2 bg-orange-500 w-36">Background</button>
+              </div>
               <CirclePicker
                 onChangeComplete={handleColorChangeBg}
+                colors={colorList}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="my-5 md:my-10 md:hidden block">
+          <div className="flex items-center md:flex-row flex-col pb-10 md:pb-8 hidden md:flex">
+            <div className="flex flex-col">
+              <h2 className="text-2xl font-bold underline whitespace-pre">
+                {bgFgSwitch ? "Background" : "Foreground"} Color:{" "}
+                {colorFg.padEnd(9, " ")}
+              </h2>
+              <div className="pb-2 flex justify-center">
+                <HuePicker
+                  className="w-full"
+                  color={colorFg || ogColorFg}
+                  onChangeComplete={handleColorChangeFg}
+                />
+              </div>
+              <button
+                className={clsx(
+                  "bg-blue-500 hover:bg-blue-700 text-white text-base md:text-lg font-bold px-4 rounded",
+                  {
+                    "opacity-70 cursor-not-allowed": bgFgSwitch ? colorFg === ogColorFg : colorBg === ogColorBg,
+                  }
+                )}
+                onClick={() => {
+                  if (colorFg !== ogColorFg) {
+                    setColorFg(ogColorFg);
+                  }
+                }}
+                disabled={
+                  bgFgSwitch ? colorBg === ogColorBg : colorFg === ogColorFg
+                }
+              >
+                Reset Foreground to White
+              </button>
+            </div>
+            <div className="ml-4 pt-4 md:p-0">
+              <CirclePicker
+                onChangeComplete={handleColorChangeFg}
+                colors={colorList}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center md:flex-row flex-col ">
+            <div className="flex flex-col">
+              <h2 className="text-2xl font-bold underline whitespace-pre">
+                {bgFgSwitch ? "Background" : "Foreground"} Color:{" "}
+                {(bgFgSwitch ? colorBg : colorFg).padEnd(9, " ")}
+              </h2>
+              <div className="pb-2 flex justify-center">
+                <HuePicker
+                  color={colorBg || ogColorBg}
+                  onChangeComplete={
+                    bgFgSwitch ? handleColorChangeBg : handleColorChangeFg
+                  }
+                />
+              </div>
+              <button
+                className={clsx(
+                  "bg-blue-500 hover:bg-blue-700 text-white text-base md:text-lg font-bold px-4 rounded",
+                  {
+                    "opacity-70 cursor-not-allowed": colorBg === ogColorBg,
+                  }
+                )}
+                onClick={() => {
+                  if (bgFgSwitch) {
+                    if (colorFg !== ogColorBg) {
+                      setColorBg(ogColorBg);
+                    }
+                  } else {
+                    if (colorFg !== ogColorFg) {
+                      setColorFg(ogColorFg);
+                    }
+                  }
+                }}
+              >
+                {bgFgSwitch
+                  ? "Reset Background to Transparent"
+                  : "Reset Foreground to White"}
+              </button>
+            </div>
+            <div className="md:ml-4 ml-0 pt-4 md:p-0 flex flex-row">
+              <div className="flex flex-col md:hidden block items-stretch content-stretch min-h-full mr-4 md:mr-0">
+                <button
+                  onClick={handleBgFgSwitch}
+                  // className="h-1/2 bg-purple-500 w-36 rounded-t"
+                  className={clsx("h-1/2 w-36 rounded-t box-border border-4", {
+                    "font-bold rounded-t border-white bg-purple-500":
+                      !bgFgSwitch,
+                    "border-transparent bg-purple-600 bg-none": bgFgSwitch,
+                  })}
+                >
+                  Foreground
+                </button>
+                <button
+                  onClick={handleBgFgSwitch}
+                  // className="h-1/2 bg-orange-500 w-36 rounded-b"
+                  className={clsx("h-1/2 w-36 rounded-b box-border border-4", {
+                    "font-bold rounded-b border-white bg-orange-500":
+                      bgFgSwitch,
+                    "border-transparent bg-orange-600 bg-none": !bgFgSwitch,
+                  })}
+                >
+                  Background
+                </button>
+              </div>
+              <CirclePicker
+                onChangeComplete={
+                  bgFgSwitch ? handleColorChangeBg : handleColorChangeFg
+                }
                 colors={colorList}
               />
             </div>
