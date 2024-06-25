011b8a15c7b01d266313ceaa25f8919e84c11362
Trillium Smith <spiteless@gmail.com>
2024-06-25 14:46:44 -0700

    feat: Update contrastTextColor to allow white/black overrides

--- src/lib/contrastTextColor.ts
+++ src/lib/contrastTextColor.ts
@@ -1,4 +1,17 @@
-export const contrastTextColor = (hexColor: string): string => {
+export const contrastTextColor = (hexColor: string, whites?: string[], blacks?: string[] ): string => {
+  // Normalize hexColor to uppercase to ensure case-insensitive comparison
+  const normalizedHexColor = hexColor.toUpperCase();
+
+  // Check if hex code is in whites array, return 'white' if found
+  if (whites && whites.map(color => color.toUpperCase()).includes(normalizedHexColor)) {
+    return 'white';
+  }
+
+  // Check if hex code is in blacks array, return 'black' if found
+  if (blacks && blacks.map(color => color.toUpperCase()).includes(normalizedHexColor)) {
+    return 'black';
+  }
+
   // Assuming hexColor is in the format "#RRGGBB"
   const color = hexColor.charAt(0) === '#' ? hexColor.substring(1, 7) : hexColor;
   const r = parseInt(color.substring(0, 2), 16); // Red value
