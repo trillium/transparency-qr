6b1c921cbe6910ed7dd4e236cf95189e439a5688
Trillium Smith <spiteless@gmail.com>
2024-06-25 21:44:30 -0700

    feat: Add white border to color swatches, add border for focus-within

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./animate.css";
+import "./circle-picker.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
