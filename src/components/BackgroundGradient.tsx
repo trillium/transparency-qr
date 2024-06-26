1a32cf7ce090ebd189d5e2989f7d5bd8ab3c3037
Trillium Smith <spiteless@gmail.com>
2024-06-25 19:06:28 -0700

    style: Reformat BackgroundGradient.tsx

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -1,16 +1,12 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 interface BackgroundGradientProps {
   children: ReactNode;
   classes?: string;
 }
 
-function BackgroundGradient({children, classes}: BackgroundGradientProps) {
-const addedClasses = classes ? ` ${classes}` : ""
-return (
-  <div className={"" + addedClasses} >
-    {children}
-  </div>
-)
+function BackgroundGradient({ children, classes }: BackgroundGradientProps) {
+  const addedClasses = classes ? ` ${classes}` : "";
+  return <div className={"" + addedClasses}>{children}</div>;
 }
 export default BackgroundGradient;
